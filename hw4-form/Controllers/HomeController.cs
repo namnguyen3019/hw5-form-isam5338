82a066fcdc15e023e7fce614c69e4854ba6906fa
Nam Nguyen <namnguyn3019@gmail.com>
2021-04-06 15:06:23 -0500

    Add Form, Submit Form and render FORM data

---
+++ hw4-form/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using hw4_form.Models;
+
+namespace hw4_form.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FormSubmit()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult GetFormDetails(string FirstName, string LastName, string StreetAddress, string ZipCode, string City, string State, Boolean FreeSample)
+        {
+            CustomerForm newCustomer = new CustomerForm();
+            newCustomer.FirstName = FirstName;
+            newCustomer.LastName = LastName;
+            newCustomer.StreetAddress = StreetAddress;
+            newCustomer.ZipCode = ZipCode;
+            newCustomer.City = City;
+            newCustomer.State = State;
+            newCustomer.FreeSample = FreeSample;
+            return View(newCustomer);
+        }
+       
+    }
+}
---
+++ hw4-form/Models/CustomerForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace hw4_form.Models
+{
+    public class CustomerForm
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string StreetAddress { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public Boolean FreeSample { get; set; }
+       
+    }
+}
